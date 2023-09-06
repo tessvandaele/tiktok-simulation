c573da322ad30ce30d5f00cfe7ec5e9909ff6394
tessvandaele <tessvandaele1@gmail.com>
2023-09-06 13:13:07 -0500

    setting up ashley

--- src/Video.js
+++ src/Video.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const USER_ID = "Superfreq";
+const USER_ID = "Ashley";
 
 // Video element with url as param
 function Video({videos, baseline, taskType}) {
