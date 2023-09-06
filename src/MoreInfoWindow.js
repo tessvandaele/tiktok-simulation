0c505ba30117e1d4fa0d6160d49e533c094ab5a6
tessvandaele <tessvandaele1@gmail.com>
2023-09-06 15:38:24 -0500

    setting up for shana

--- src/MoreInfoWindow.js
+++ src/MoreInfoWindow.js
@@ -13,7 +13,7 @@ function MoreInfoWindow({ video, videoRef, taskType }) {
     const openModal = () => { setIsOpen(true) };
     const closeModal = () => { setIsOpen(false) };
 
-    const USER_ID = "Ashley";
+    const USER_ID = "Shana";
 
     function logEvent(userId, interaction, element, video_id, video_time, task_type) {
         const db = getDatabase();
