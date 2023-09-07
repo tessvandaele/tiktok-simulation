0c35b6a6a91d743d1a0cabe4ae37d7f17f560638
Akhil Iyer <akhil.iyer1@gmail.com>
2023-09-07 16:51:27 -0500

    Switched name for upcoming interview

--- src/Video.js
+++ src/Video.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const USER_ID = "Kaley";
+const USER_ID = "Lewis";
 
 // Video element with url as param
 function Video({videos, baseline, taskType}) {
