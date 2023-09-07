a8b69a68417e7246461065d6d6aa5843fff2b557
tessvandaele <tessvandaele1@gmail.com>
2023-09-07 14:57:48 -0500

    setting up for kaley

--- src/Video.js
+++ src/Video.js
@@ -25,7 +25,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const USER_ID = "Karen";
+const USER_ID = "Kaley";
 
 // Video element with url as param
 function Video({videos, baseline, taskType}) {
